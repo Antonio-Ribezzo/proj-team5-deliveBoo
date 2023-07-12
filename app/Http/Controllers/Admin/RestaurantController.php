994d99dbfd5b9219b700409cbf7719452b7d65a1
RiccardoMartella <riccardomartella77@gmail.com>
2023-07-12 17:59:00 +0200

    one to one error

---
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+}
